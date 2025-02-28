9b405fa74638c871a676c107904326c567a0b46a
naviya30 <135148797+naviya30@users.noreply.github.com>
2025-02-28 19:32:37 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
+  email: "naveenyatawara30@gmail.com",
   password: "Enter your mega password",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
